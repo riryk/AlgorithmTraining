7b91fcbefbd3a01147116f0fc6ee83505ad79c2d
Ruslan Iryk <Ruslan_Iryk@epam.com>
2019-04-22 18:16:22 +0300

    finished islands

--- AlgorithmTraining/IOI/2008/islands.cpp
+++ AlgorithmTraining/IOI/2008/islands.cpp
@@ -19,6 +19,46 @@ pll dp[N];
 int deg[N];
 long long dm[N];
 
+long long islands_solve(int u) 
+{
+    long long ret; 
+	vector<pll> V;
+	deque<pll> Q;
+	V.push_back(pll(0, 0));
+	while (deg[u])
+	{
+       deg[u]--;
+	   dm[u] = max(dm[u], dp[u].first + dp[u].second);
+	   ret = max(ret, dm[u]);
+	   V.push_back(pll(u, par[u].second));
+	   u = par[u].first;
+	}
+
+	int sz = V.size() - 1;
+	for (int i = 1; i <= sz; i++) V[i].second += V[i - 1].second;
+
+	long long longest_subpath;
+	for (int i = 2; i <= sz; i++) 
+	{
+		longest_subpath = V[i - 1].second + dp[V[i].first].first;
+		while(!Q.empty() && Q.back().first < longest_subpath) Q.pop_back();
+		Q.push_back(pll(longest_subpath, i));
+	}
+
+	ret = max(ret, dp[V[1].first].first + Q.front().first);
+
+    for (int i = 1; i < sz; i++) 
+	{
+		if(Q.front().second == i + 1) Q.pop_front();
+		longest_subpath = V[sz].second + V[i - 1].second + dp[V[i].first].first;
+		while(!Q.empty() && Q.back().first < longest_subpath) Q.pop_back();
+		Q.push_back(pll(longest_subpath, i));
+		ret = max(ret, dp[V[i + 1].first].first + Q.front().first - V[i].second);
+	}
+
+	return ret;
+}
+
 void islands()
 {
 	scanf("%d", in);
@@ -35,11 +75,18 @@ void islands()
 	{
 		int now = Q.front();
 		Q.pop_front();
+
+		long long p = par[now].first, w = par[now].second;
 		dm[now] = max(dm[now], dp[now].first + dp[now].second);
-		dm[par[now].first] = max(dp[par[now].first].first, dm[now]);
-		long long path = dp[now].first + par[now].second;
-		if (path > dp[par[now].first].first) swap(path, dp[par[now].first].first);
-		if (path > dp[par[now].first].second) swap(path, dp[par[now].first].second);
+		dm[p] = max(dm[p], dm[now]);
+ 
+		long long ret = dp[now].first + w;
+		if(ret > dp[p].first) swap(ret, dp[p].first);
+		if(ret > dp[p].second) swap(ret, dp[p].second);
+
 		if (!--deg[par[now].first]) Q.push_back(par[now].first);
 	}
+
+	long long ans = 0;
+	for(int i = 1; i <= in; i++) if(deg[i]) ans += islands_solve(i);
 }
