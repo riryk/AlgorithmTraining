d622796a913584e3b50ce909d1f40022a03948c9
Ruslan Iryk <Ruslan_Iryk@epam.com>
2019-12-16 20:04:43 +0200

    finished with salesman

--- AlgorithmTraining/IOI/2009/salesman.cpp
+++ AlgorithmTraining/IOI/2009/salesman.cpp
@@ -16,6 +16,7 @@ using namespace std;
 namespace salesman
 {
 	const int MX=500010;
+    const int XMX=500001;
 	const long long linf=(1LL<<60);
 
 	int n, d, u, s; 
@@ -43,14 +44,55 @@ namespace salesman
             tree[v]=max(tree[v<<1],tree[(v<<1)+1]);
 		}
 		void upt(int pos, long long val){
-           upt(1,1,n,pos,val);
+            upt(1,1,n,pos,val);
         }
+		long long mmax(int v, int s, int e, int l, int r) {
+            if (l>e || r<s) return linf;
+			if (s<=l && r<=e) return tree[v];
+			int m=(s+e)>>1;
+            return max(mmax(v<<1,s,m,l,r), mmax((v<<1)+1,m+1,e,l,r));
+		}
+        long long max(int l, int r) {
+			if (r<l) return linf;
+			return mmax(1,1,n,l,r); 
+		}
 	} US, DS;
 
 	int comp(struct shop s1, struct shop s2)
 	{
 		if (s1.t==s2.t) return s1.x<s2.x;
 		return s1.t<s2.t;
+	}
+    
+	long long get(int x)
+	{
+		long long update_max = US.max(x,XMX)+u*x;
+		long long down_max = DS.max(1,x)-d*x;
+		return max(update_max, down_max);
+	}
+
+	long long T[MX];
+    long long X[MX], Y[MX];
+
+	void check(int l, int r)
+	{
+        for (int i=l; i<=r; i++) 
+			T[i]=get(S[i].x);
+
+		X[l]=T[l]+S[l].a;
+		Y[r]=T[r]+S[r].a;
+        
+		for(int i=l+1; i<=r; i++)
+           X[i]=max(X[i-1]-d*(S[i].x-S[i-1].x), T[i])+S[i].a;
+
+        for(int i=r-1; i>=l; i--)
+           Y[i]=max(Y[i+1]-u*(S[i+1].x-S[i].x), T[i])+S[i].a;
+
+        for (int i=1; i<=r; i++)
+		{
+			US.upt(S[i].x, max(X[i], Y[i]) - u*S[i].x);
+            DS.upt(S[i].x, max(X[i], Y[i]) + d*S[i].x);
+		}
 	}
 
     void solve()
@@ -71,5 +113,12 @@ namespace salesman
         US.init(500001);
 		US.upt(s, -u*s);
         DS.upt(s, d*s);
+        int pos=1;
+        while(pos<=n){
+           int l=pos,r=pos;
+		   while(r<n && S[r+1].t==S[l].t) r++;
+           check(l,r);
+           pos=r+1;
+		}
 	}
 }
