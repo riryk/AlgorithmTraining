d6eaed48a90f3a7784de329c1017bd9d8119a9a7
Ruslan Iryk <Ruslan_Iryk@epam.com>
2020-01-16 19:47:22 +0200

    finished boundary

--- AlgorithmTraining/IOI/2003/boundary.cpp
+++ AlgorithmTraining/IOI/2003/boundary.cpp
@@ -18,6 +18,7 @@ using namespace std;
 namespace boundary
 { 
    int r,n,x,y;
+   int l[2000001];
 
    pair<double,double> getp(int x1,int y1,int x2,int y2){
        x2 -= x1;
@@ -38,6 +39,23 @@ namespace boundary
 
 	   return f;
    }
+   
+   double getv(double x, double y)
+   {
+       if (x == 0 && y == 0) return 0;
+       if (y == 0) return x;
+	   if (y == n) return 3*n - x;
+	   if (x == n) return n + y;
+	   return 4*n - y;
+   }
+   
+   int up(double d) {
+	   return (int)(ceil(d) + 0.1);
+   }
+
+   int down(double d) {
+	   return (int)d;
+   }
 
    void solve()
    {
@@ -45,10 +63,28 @@ namespace boundary
 	   for (size_t i=0;i<r;i++){
            int p;
 		   cin>>p;
+		   double lv;
 		   for (size_t j=0;j<p;j++){
               int a,b;
 			  cin>>a>>b;
               pair<double, double> np = getp(x, y, a, b);
+			  double nv = getv(np.first, np.second);
+			  pair<double, double> inters = getp(np.first, np.second, x, y);
+              double iv = getv(np.first, np.second);
+			  if (j != 0) {
+                 double q = min(lv, nv);
+				 double w = max(lv, nv);
+				 if (!(q <= iv && iv <= w)){
+                    ++l[up(q)];
+					--l[down(w) + 1];
+				 } else {
+					++l[0];
+					--l[down(q) + 1];
+					++l[up(w)];
+					--l[4 * n];
+				 }              
+			  }
+			  lv=nv;
 		   }
 	   }
    }
