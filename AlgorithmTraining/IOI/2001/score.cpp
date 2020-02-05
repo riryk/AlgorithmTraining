07b88ce2c2d9d79c760faf6dc8eaadf7dec26498
Ruslan Iryk <Ruslan_Iryk@epam.com>
2020-02-05 18:38:32 +0200

    finished score

--- AlgorithmTraining/IOI/2001/score.cpp
+++ AlgorithmTraining/IOI/2001/score.cpp
@@ -42,7 +42,49 @@ namespace score
                 d = Max2[*it];
 				c = Max1[*it];
 			}
+			if (a){
+				if (B[x] == 1) {
+                    Max1[x] = max(a, V[x]);
+                    Max2[x] = b;
+				}
+				else {
+                    Max1[x] = a;
+                    Max2[x] = max(b, V[x]);
+                }
+			}
+			else {
+				if (B[x] == 1) {
+                    Max1[x] = max(c, V[x]);
+                    Max2[x] = d;
+				}
+				else {
+                    Max1[x] = c;
+                    Max2[x] = max(d, V[x]);
+				}
+			}
 		}
+	}
+
+	int find(int x)
+    {
+        int maxi(0),a,i,mini(123123123),b,t;
+
+        for (i=0; i<=(int)G[x].size(); i++)
+		{
+             t = G[x][i];
+             if (Max1[t] > Max2[t] && Max1[t] > maxi)
+             {
+                 maxi = Max1[t];
+                 a = i;
+             }
+             if (Max2[t] < mini)
+             {
+                 mini = Max2[t];
+                 b = i;
+             }
+		}
+
+		return maxi ? G[x][a] : G[x][b];
 	}
 
     void solve()
