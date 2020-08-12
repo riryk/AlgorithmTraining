25eb2fb1cf1b8e50932b1d3970d8432aa6fe4622
Ruslan Iryk <Ruslan_Iryk@epam.com>
2020-08-12 18:56:43 +0300

    finished rails

---
+++ AlgorithmTraining/IOI/2014/rails.cpp
@@ -0,0 +1,22 @@
+#include <vector>  
+#include <errno.h>
+#include <math.h>  
+#include <algorithm> 
+#include <set>
+#include <algorithm>
+#include <iostream>
+#include <cassert>
+#include <cstdio>
+#include <cstring>
+#include <vector>
+#include <map>
+#include <utility>
+#include "rails.h"
+
+using namespace System;
+using namespace std;
+ 
+namespace rails
+{
+}
+
---
+++ AlgorithmTraining/IOI/2014/rails.h
@@ -0,0 +1,9 @@
+#ifndef ioi_2014_rails_h
+#define ioi_2014_rails_h
+
+namespace rails
+{
+	void test();
+}
+
+#endif
