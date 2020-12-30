a24b0e9ce7dcd715532d7e3b6322e3c4551ad607
ruslan iryk <ruslan_iryk@epam.com>
2020-12-30 21:10:59 +0200

    start bug

---
+++ AlgorithmTraining/IOI/2016/bug_small.cpp
@@ -0,0 +1,37 @@
+#include <vector>  
+#include <errno.h>
+#include <math.h>  
+#include <algorithm> 
+#include <set>
+#include <algorithm>
+#include <iostream>
+#include <cassert>
+#include <cstdio>
+#include <cstring>
+#include <vector>
+#include <map>
+#include <utility>
+#include "bug.h"
+
+using namespace System;
+using namespace std;
+ 
+namespace bug
+{
+    void add_element(string s)
+	{
+	}
+
+    vector<int> restore_permutations(int n, int w, int r)
+    {
+		for (int i = 0; i < n; i++)
+		{
+			string s;
+            
+		}
+
+        vector<int> res(n);
+		return res;
+    }
+}
+
