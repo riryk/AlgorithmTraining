f36e854d50e44b442fc33970032c01c67a97ff68
ruslan iryk <ruslan_iryk@epam.com>
2021-04-15 16:56:19 +0300

    continue aliens

---
+++ AlgorithmTraining/IOI/2016/aliens_convex_hull_trick.cpp
@@ -0,0 +1,25 @@
+#include <vector>  
+#include <errno.h>
+#include <math.h>  
+#include <algorithm> 
+#include <set>
+#include <algorithm>
+#include <iostream>
+#include <cassert>
+#include <cstdio>
+#include <cstring>
+#include <vector>
+#include <map>
+#include <utility>
+#include "aliens.h"
+
+using namespace System;
+using namespace std;
+
+typedef long long ll;
+
+namespace aliens
+{
+   
+}
+
